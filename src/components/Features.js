3a2a90c655b474bb09e00936c6c67400793e49b7
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-03-06 13:06:54 +0530

    feature icons and first lines removed

--- src/components/Features.js
+++ src/components/Features.js
@@ -15,6 +15,8 @@ import reportingrate from "../images/HTML USE IMAGE/reportingrate.png"
 import coolingsystem from "../images/HTML USE IMAGE/coolingsystem.png"
 import touch from "../images/HTML USE IMAGE/touch.png"
 import wifi from "../images/HTML USE IMAGE/online.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMicrochip ,faBatteryThreeQuarters, faMobileAlt, faPercentage, faFileAlt, faThermometerHalf, faWifi, faHandPointer} from '@fortawesome/free-solid-svg-icons'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,14 +26,14 @@ const useStyles = makeStyles(theme => ({
 
 const Features = () => {
   const [features, setFeatures] = useState([
-    ["3300 mAh Polymer Battery", battery_],
-    ['5.7" 2K+ Immersive Display', display],
-    ["Qualcomm Snapdragon 821", snapdragon],
-    ["60Hz Higher Refer Rate", rate],
-    ["120Hz Touch Reporting Rate", reportingrate],
-    ["Liquid Cooling System", coolingsystem],
-    ["Master Touch", touch],
-    ["SA+NSA 4G WiFi6", wifi],
+    ["3300 mAh Polymer Battery", faBatteryThreeQuarters],
+    ['5.7" 2K+ Immersive Display', faMobileAlt],
+    ["Qualcomm Snapdragon 821", faMicrochip],
+    ["60Hz Higher Refer Rate", faPercentage],
+    ["120Hz Touch Reporting Rate", faFileAlt],
+    ["Liquid Cooling System", faThermometerHalf],
+    ["Master Touch", faHandPointer],
+    ["SA+NSA 4G WiFi6", faWifi],
   ])
   const classes = useStyles()
   return (
@@ -63,11 +65,11 @@ const Features = () => {
           xl={12}
         >
           {features.slice(0, 4).map((value, i) => (
-            <Grid item xl={3} lg={3} md={3} sm={3} xs={12}>
-              <div style={{marginTop: "5em"}}>
+            <Grid item xl={3} lg={3} md={3} sm={12} xs={12}>
+              <div style={{marginTop: "5em"}} className="feature-icon_">
                 <center>
-                  <div className={i === 2 ? "snapdragon" : "feature-icon"}>
-                    <img src={value[1]} />
+                  <div>
+                    <FontAwesomeIcon icon={value[1]} size="2x" className="featureicon" /> 
                   </div>
                 </center>
 
@@ -76,31 +78,23 @@ const Features = () => {
                     color: "white",
                     textAlign: "center",
                     paddingTop: "5em",
+                    margin: "2em",
+                    whiteSpace: "nowrap"
                   }}
-                  className={i === 2 ? "snapdragonp" : "featuretext"}
+                  className="featuretext"
                 >
                   {value[0]}
                 </p>
               </div>
-              {<p className="line"></p>}
+              {i == 0 ? null : <p className="line"></p>}
             </Grid>
           ))}
           {features.slice(4).map((value, i) => (
-            <Grid item xl={3} lg={3} md={3} sm={3} xs={12}>
+            <Grid item xl={3} lg={3} md={3} sm={12} xs={12}>
               <div>
                 <center>
-                  <div
-                    className={
-                      i === 0
-                        ? "reportingrate"
-                        : i === 1
-                        ? "cooling"
-                        : i === 2
-                        ? "touch"
-                        : "wifi"
-                    }
-                  >
-                    <img src={value[1]} />
+                  <div>
+<FontAwesomeIcon icon={value[1]} size="2x" className="featureicon" /> 
                   </div>
                 </center>
                 <p
@@ -108,21 +102,13 @@ const Features = () => {
                     color: "white",
                     textAlign: "center",
                     paddingTop: "5em",
+                    whiteSpace: "nowrap"
                   }}
-                  className={
-                    i === 0
-                      ? "reportingratep"
-                      : i === 1
-                      ? "coolingp"
-                      : i === 3
-                      ? "wifip"
-                      : "featuretext"
-                  }
                 >
                   {value[0]}
                 </p>
               </div>
-              {<p className="line"></p>}
+              {i == 0 ? null : <p className="line"></p>}
             </Grid>
           ))}
         </Grid>
