0503c4a9d0a3ee508f8fa888ab8290f6d71b0536
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-03-08 17:44:14 +0530

    Updates

---
+++ component---src-pages-contact-js-77e308e7c1912884116a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a),i=(n("Wbzz"),n("Bl7J")),l=n("vrFN"),u=n("PU0c");t.default=function(){return Object(a.useEffect)((function(){window.pageExitTime=1e3}),[]),c.a.createElement(i.a,null,c.a.createElement(l.a,{title:"Home"}),c.a.createElement("h4",{id:"heading"},"Contact Us"),c.a.createElement(u.a,null))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-77e308e7c1912884116a.js.map
