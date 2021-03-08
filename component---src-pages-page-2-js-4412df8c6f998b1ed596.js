b846aa2f66df31b4864bbf8f4add07f1a1fd8c46
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-03-09 03:44:04 +0530

    Updates

---
+++ component---src-pages-page-2-js-4412df8c6f998b1ed596.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{"7wMf":function(e,n,t){e.exports=t.p+"static/loading-0780af73c2e177f46e9d70363541e947.gif"},p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),i=t.n(a),c=(t("Wbzz"),t("Bl7J"),t("7wMf")),o=t.n(c);n.default=function(){return i.a.createElement("div",{id:"loadingscreen"},i.a.createElement("img",{src:o.a}))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-4412df8c6f998b1ed596.js.map
