3d627601cd055a398369f331116989d436203304
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-03-09 08:28:47 +0530

    bugfix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,14 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import PagesHome from "../components/PagesHome.js"
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
       <h4 id="heading">Contact Us</h4>
-      <PagesHome />
     </Layout>
   )
 }
